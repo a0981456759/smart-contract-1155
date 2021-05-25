7760b549d6c4c7ac94511a97f8437f9084f1282a
Chenhsuan Wang <43899116+a0981456759@users.noreply.github.com>
2021-05-26 03:57:30 +0800

    Add files via upload

---
+++ smart-contract/Erc1155.sol
@@ -0,0 +1,432 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/utils/Strings.sol";
+import {SafeMath} from  "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+
+abstract contract ContextMixin {
+    function msgSender()
+        internal
+        view
+        returns (address payable sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = payable(msg.sender);
+        }
+        return sender;
+    }
+}
+
+contract Initializable {
+    bool inited = false;
+
+    modifier initializer() {
+        require(!inited, "already inited");
+        _;
+        inited = true;
+    }
+}
+
+
+contract EIP712Base1 is Initializable {
+    struct EIP712Domain {
+        string name;
+        string version;
+        address verifyingContract;
+        bytes32 salt;
+    }
+
+    string constant public ERC712_VERSION = "1";
+
+    bytes32 internal constant EIP712_DOMAIN_TYPEHASH = keccak256(
+        bytes(
+            "EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)"
+        )
+    );
+    bytes32 internal domainSeperator;
+
+    // supposed to be called once while initializing.
+    // one of the contracts that inherits this contract follows proxy pattern
+    // so it is not possible to do this in a constructor
+    function _initializeEIP712(
+        string memory name
+    )
+        internal
+        initializer
+    {
+        _setDomainSeperator(name);
+    }
+
+    function _setDomainSeperator(string memory name) internal {
+        domainSeperator = keccak256(
+            abi.encode(
+                EIP712_DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                keccak256(bytes(ERC712_VERSION)),
+                address(this),
+                bytes32(getChainId())
+            )
+        );
+    }
+
+    function getDomainSeperator() public view returns (bytes32) {
+        return domainSeperator;
+    }
+
+    function getChainId() public view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
+    }
+
+    /**
+     * Accept message hash and returns hash message in EIP712 compatible form
+     * So that it can be used to recover signer from signature signed using EIP712 formatted data
+     * https://eips.ethereum.org/EIPS/eip-712
+     * "\\x19" makes the encoding deterministic
+     * "\\x01" is the version byte to make it compatible to EIP-191
+     */
+    function toTypedMessageHash(bytes32 messageHash)
+        internal
+        view
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encodePacked("\x19\x01", getDomainSeperator(), messageHash)
+            );
+    }
+}
+
+
+
+
+contract NativeMetaTransaction is EIP712Base1 {
+    using SafeMath for uint256;
+    bytes32 private constant META_TRANSACTION_TYPEHASH = keccak256(
+        bytes(
+            "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
+        )
+    );
+    event MetaTransactionExecuted(
+        address userAddress,
+        address payable relayerAddress,
+        bytes functionSignature
+    );
+    mapping(address => uint256) nonces;
+
+    /*
+     * Meta transaction structure.
+     * No point of including value field here as if user is doing value transfer then he has the funds to pay for gas
+     * He should call the desired function directly in that case.
+     */
+    struct MetaTransaction {
+        uint256 nonce;
+        address from;
+        bytes functionSignature;
+    }
+
+    function executeMetaTransaction(
+        address userAddress,
+        bytes memory functionSignature,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    ) public payable returns (bytes memory) {
+        MetaTransaction memory metaTx = MetaTransaction({
+            nonce: nonces[userAddress],
+            from: userAddress,
+            functionSignature: functionSignature
+        });
+
+        require(
+            verify(userAddress, metaTx, sigR, sigS, sigV),
+            "Signer and signature do not match"
+        );
+
+        // increase nonce for user (to avoid re-use)
+        nonces[userAddress] = nonces[userAddress].add(1);
+
+        emit MetaTransactionExecuted(
+            userAddress,
+            payable(msg.sender),
+            functionSignature
+        );
+
+        // Append userAddress and relayer address at the end to extract it from calling context
+        (bool success, bytes memory returnData) = address(this).call(
+            abi.encodePacked(functionSignature, userAddress)
+        );
+        require(success, "Function call not successful");
+
+        return returnData;
+    }
+
+    function hashMetaTransaction(MetaTransaction memory metaTx)
+        internal
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encode(
+                    META_TRANSACTION_TYPEHASH,
+                    metaTx.nonce,
+                    metaTx.from,
+                    keccak256(metaTx.functionSignature)
+                )
+            );
+    }
+
+    function getNonce(address user) public view returns (uint256 nonce) {
+        nonce = nonces[user];
+    }
+
+    function verify(
+        address signer,
+        MetaTransaction memory metaTx,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    ) internal view returns (bool) {
+        require(signer != address(0), "NativeMetaTransaction: INVALID_SIGNER");
+        return
+            signer ==
+            ecrecover(
+                toTypedMessageHash(hashMetaTransaction(metaTx)),
+                sigV,
+                sigR,
+                sigS
+            );
+    }
+}
+
+contract ERC1155Tradable is ContextMixin, ERC1155, NativeMetaTransaction, Ownable {
+  using Strings for string;
+  using SafeMath for uint256;
+
+//   address proxyRegistryAddress;
+  mapping (uint256 => address) public creators;
+  mapping (uint256 => uint256) public tokenSupply;
+  mapping (uint256 => string) customUri;
+  // Contract name
+  string public name;
+  // Contract symbol
+  string public symbol;
+  /**
+   * @dev Require _msgSender() to be the creator of the token id
+   */
+  modifier creatorOnly(uint256 _id) {
+    require(creators[_id] == _msgSender(), "ERC1155Tradable#creatorOnly: ONLY_CREATOR_ALLOWED");
+    _;
+  }
+
+  /**
+   * @dev Require _msgSender() to own more than 0 of the token id
+   */
+  modifier ownersOnly(uint256 _id) {
+    require(balanceOf(_msgSender(), _id) > 0, "ERC1155Tradable#ownersOnly: ONLY_OWNERS_ALLOWED");
+    _;
+  }
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _uri
+  ) ERC1155(_uri) {
+    name = _name;
+    symbol = _symbol;
+    _initializeEIP712(name);
+  }
+
+  function uri(
+    uint256 _id
+  ) override public view returns (string memory) {
+    require(_exists(_id), "ERC1155Tradable#uri: NONEXISTENT_TOKEN");
+    // We have to convert string to bytes to check for existence
+    bytes memory customUriBytes = bytes(customUri[_id]);
+    if (customUriBytes.length > 0) {
+        return customUri[_id];
+    } else {
+        return super.uri(_id);
+    }
+  }
+
+  /**
+    * @dev Returns the total quantity for a token ID
+    * @param _id uint256 ID of the token to query
+    * @return amount of token in existence
+    */
+  function totalSupply(
+    uint256 _id
+  ) public view returns (uint256) {
+    return tokenSupply[_id];
+  }
+
+  /**
+   * @dev Sets a new URI for all token types, by relying on the token type ID
+    * substitution mechanism
+    * https://eips.ethereum.org/EIPS/eip-1155#metadata[defined in the EIP].
+   * @param _newURI New URI for all tokens
+   */
+  function setURI(
+    string memory _newURI
+  ) public onlyOwner {
+    _setURI(_newURI);
+  }
+
+  /**
+   * @dev Will update the base URI for the token
+   * @param _tokenId The token to update. _msgSender() must be its creator.
+   * @param _newURI New URI for the token.
+   */
+  function setCustomURI(
+    uint256 _tokenId,
+    string memory _newURI
+  ) public {
+    customUri[_tokenId] = _newURI;
+    emit URI(_newURI, _tokenId);
+  }
+
+
+  /**
+    * @dev Mints some amount of tokens to an address
+    * @param _to          Address of the future owner of the token
+    * @param _id          Token ID to mint
+    * @param _quantity    Amount of tokens to mint
+    * @param _data        Data to pass if receiver is contract
+    */
+  function mint(
+    address _to,
+    uint256 _id,
+    uint256 _quantity,
+    string memory metadata,
+    bytes memory _data
+    )public returns (uint256){
+        creators[_id] = _msgSender();
+        if (bytes(metadata).length > 0) {
+          customUri[_id] = metadata;
+          emit URI(metadata, _id);
+        }
+    _mint(_to, _id, _quantity, _data);
+    tokenSupply[_id] = tokenSupply[_id].add(_quantity);
+    return _id;
+    }
+
+
+  /**
+    * @dev Mint tokens for each id in _ids
+    * @param _to          The address to mint tokens to
+    * @param _ids         Array of ids to mint
+    * @param _quantities  Array of amounts of tokens to mint per id
+    * @param _data        Data to pass if receiver is contract
+    */
+  function batchMint(
+    address _to,
+    uint256[] memory _ids,
+    uint256[] memory _quantities,
+    string []memory _metadata,
+    bytes memory _data
+  ) public {
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 _id = _ids[i];
+      creators[_id] = _msgSender();
+      if(bytes(_metadata[i]).length >0){
+          customUri[_id] = _metadata[i];
+          emit URI(_metadata[i], _id);
+      }
+    //   require(creators[_id] == _msgSender(), "ERC1155Tradable#batchMint: ONLY_CREATOR_ALLOWED");
+      uint256 quantity = _quantities[i];
+      tokenSupply[_id] = tokenSupply[_id].add(quantity);
+    }
+    _mintBatch(_to, _ids, _quantities, _data);
+    
+  }
+
+  /**
+    * @dev Change the creator address for given tokens
+    * @param _to   Address of the new creator
+    * @param _ids  Array of Token IDs to change creator
+    */
+  function setCreator(
+    address _to,
+    uint256[] memory _ids
+  ) public {
+    require(_to != address(0), "ERC1155Tradable#setCreator: INVALID_ADDRESS.");
+    for (uint256 i = 0; i < _ids.length; i++) {
+      uint256 id = _ids[i];
+      _setCreator(_to, id);
+    }
+  }
+
+  /**
+   * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-free listings.
+   */
+  function isApprovedForAll(
+    address _owner,
+    address _operator
+  ) override public view returns (bool isOperator) {
+    // Whitelist OpenSea proxy contract for easy trading.
+    // // ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+    if (address(_owner) == _operator) {
+      return true;
+    }
+
+    return ERC1155.isApprovedForAll(_owner, _operator);
+  }
+
+  /**
+    * @dev Change the creator address for given token
+    * @param _to   Address of the new creator
+    * @param _id  Token IDs to change creator of
+    */
+  function _setCreator(address _to, uint256 _id) internal creatorOnly(_id)
+  {
+      creators[_id] = _to;
+  }
+
+  /**
+    * @dev Returns whether the specified token exists by checking to see if it has a creator
+    * @param _id uint256 ID of the token to query the existence of
+    * @return bool whether the token exists
+    */
+  function _exists(
+    uint256 _id
+  ) internal view returns (bool) {
+    return creators[_id] != address(0);
+  }
+
+  function exists(
+    uint256 _id
+  ) external view returns (bool) {
+    return _exists(_id);
+  }
+
+    /**
+     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+     */
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address sender)
+    {
+        return ContextMixin.msgSender();
+    }
+}
